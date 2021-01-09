ba7ba0a89037a3135ebca4ca9c84c5ed0d8c3035
Patryk Sulej <01130076@pw.edu.pl>
2021-01-09 15:24:30 +0100

    zmiany commitowane

--- Adding_meetings.cs
+++ Adding_meetings.cs
@@ -91,11 +91,11 @@ namespace Panel_dodawania_spotkania
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(116, 93);
+            this.button1.Location = new System.Drawing.Point(74, 88);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(156, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Testowy buttonn";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
