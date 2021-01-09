6d9d0cda53ba2c8c934ffb0815ab9a9e961422f5
Patryk Sulej <01130076@pw.edu.pl>
2021-01-09 16:21:33 +0100

    testowy pull

--- testowy.Designer.cs
+++ testowy.Designer.cs
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonTESTOWY1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonTESTOWY1
+            // 
+            this.buttonTESTOWY1.Location = new System.Drawing.Point(251, 136);
+            this.buttonTESTOWY1.Name = "buttonTESTOWY1";
+            this.buttonTESTOWY1.Size = new System.Drawing.Size(200, 72);
+            this.buttonTESTOWY1.TabIndex = 0;
+            this.buttonTESTOWY1.Text = "button1";
+            this.buttonTESTOWY1.UseVisualStyleBackColor = true;
+            this.buttonTESTOWY1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // testowy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonTESTOWY1);
+            this.Name = "testowy";
             this.Text = "testowy";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonTESTOWY1;
     }
 }
